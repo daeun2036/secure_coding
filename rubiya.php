c5ec8cf91bb7e2a8320a0a14e940a7e77de4eb91
daeun2036 <moderato2036@naver.com>
2022-07-22 15:14:45 +0900

    html tag

--- rubiya.php
+++ rubiya.php
@@ -80,7 +80,8 @@ if($_GET['page'] == "download"){
         }
     }
     //$escape_file=strip_tags($escape_file);
-    $content = strip_tags(file_get_contents("./upload/{$escape_file}"));
+    $escape_file = htmlspecialchars($escape_file);
+    $content = file_get_contents("./upload/{$escape_file}");
     if(!$content){
         exit("<script>alert(`not exists file`);history.go(-1);</script>");
     }
